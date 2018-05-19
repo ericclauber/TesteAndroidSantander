b418b09769438e3f2e822a3144003760c93905a2
Eric Clauber <ericclauberic@gmail.com>
2018-05-19 14:59:59 -0300

    Iniciando projeto e configuracao de Build e constant

---
+++ app/src/main/java/com/example/eric/testeandroidsantander/webservices/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.eric.testeandroidsantander.webservices
+
+import com.example.eric.testeandroidsantander.BuildConfig
+
+/**
+ * Created by eric on 19/05/18.
+ */
+object Constants {
+
+    val URL_CELLS = BuildConfig.URL_CELLS
+    val URL_FUND = BuildConfig.URL_FUND
+}
